c32c9f7415d946505e4afb30d30d41de12b1b0c9
RioAgasta <rioagasta@gmail.com>
2022-10-11 14:44:43 +0700

    Revert Attempt

--- lib/data/shared_prefs.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPrefs {
-  static Future<SharedPreferences> getPrefs() async => await SharedPreferences.getInstance();
-
-  static Future setInt(String key, int value) async => (await getPrefs()).setInt(key, value);
-  static Future<int?> getInt(String key) async => (await getPrefs()).getInt(key);
-}
